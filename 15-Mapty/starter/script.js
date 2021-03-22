e648563f057b0f3ee69ab22cfe72a555b9dafa45
OM PRAKASH <65801982+omprakash-sahoo@users.noreply.github.com>
2021-03-22 18:18:34 +0530

    Update script.js

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation){
+	navigator.geolocation.getCurrentPosition(function(position){
+		// const lattitude = position.coords.latitude
+		// const longitude = position.coords.longitude
+		//------we can write like this structuring -------
+		const {latitude} = position.coords;
+		const {longitude} = position.coords;
+		console.log(`${latitude} kkkk ${longitude}`)
+		console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`)
+},function(){
+	alert('Could not get your position'); 
+})
