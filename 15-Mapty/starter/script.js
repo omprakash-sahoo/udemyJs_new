ec792951b3a4337752ad413c6a8b80fe603bfedc
OM PRAKASH <65801982+omprakash-sahoo@users.noreply.github.com>
2021-03-29 22:30:13 +0530

    Update script.js
    popup added

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -19,7 +19,33 @@ if (navigator.geolocation){
 		const {latitude} = position.coords;
 		const {longitude} = position.coords;
 		console.log(`${latitude} kkkk ${longitude}`)
-		console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`)
+		console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`);
+        const coords = [latitude,longitude];
+
+        var map = L.map('map').setView(coords, 18);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+			map.on('click',function(mapEvent){
+				console.log(mapEvent);
+				// const lat = mapEvent.latlng.lat;
+				// const lng = mapEvent.latlng.lng;
+				const {lat,lng} = mapEvent.latlng;
+			L.marker([lat,lng]).addTo(map).bindPopup(
+				L.popup({
+					maxWidth:250,
+					minWidth:100,
+					autoClose:false,
+					closeOnClick:false,
+					className:'running-popup'
+				})
+			)
+            .setPopupContent('Workout')
+            .openPopup();
+				
+			})
 },function(){
 	alert('Could not get your position'); 
 });
+}
